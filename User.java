14452bae18fee06a407b7f01f3ed9e1899303946
Wangzy673234096 <33142074+Wangzy673234096@users.noreply.github.com>
2017-10-27 15:05:46 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,28 @@
+package ChatSystem;
+
+public class User {
+	/**
+	 * 用户信息类
+	 * 用于记录用户个人信息：姓名以及IP
+	 */
+	private String name;
+	private String ip;
+	public User(String name, String ip) {
+		super();
+		this.name = name;
+		this.ip = ip;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+}
